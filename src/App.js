6549be88111069bc6f594762a9d671ecfa54b577
Monroe Gollaher <mgollaher92@gmail.com>
2021-01-26 15:53:07 -0700

    big style change, changed how data is fetched

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import { getPokemon, setPokemon } from "./services/PokemonService";
-import Card from './components/Card'; 
+import Card from './components/Card/Card'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [pokemonData, setPokemonData] = useState([]);
   const [nextUrl, setNextUrl] = useState('');
-  const [preUrl, setPrevUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
   const [loading, setLoading] = useState(true);
   const initUrl = 'https://pokeapi.co/api/v2/pokemon'; 
 
@@ -15,31 +16,62 @@ function App() {
       let res = await getPokemon(initUrl);
       setNextUrl(res.next);
       setPrevUrl(res.previous);
-      await loadingPokemon(res.results);
+      await loadPokemon(res.results);
       setLoading(false);
     }
     fetchData();
   }, []); 
 
-  const loadingPokemon = async (data) => {
+  const loadPokemon = async (data) => {
     let _pokemonData = await Promise.all(data.map(async pokemon => {
       let pokemonRecord = await setPokemon(pokemon.url);
       return pokemonRecord;
     }))
-
     setPokemonData(_pokemonData);
+  }
+
+  const next = async () => {
+    setLoading(true);
+    let data = await getPokemon(nextUrl);
+    await loadPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  }
+
+  const prev = async () => {
+    if (!prevUrl) return;
+    setLoading(true);
+    let data = await getPokemon(prevUrl);
+    await loadPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
   }
   console.log(pokemonData);
   return (
     <div className="App">
       { 
-        loading  ? <h1>Loading...</h1> : (
+        loading  ? <h1>Loading Pokemon...</h1> : (
           <>
-          <div className="grid-container">
-            { pokemonData.map((pokemon, i) => {
-              return <Card key={i} pokemon={pokemon} />
+          <div className="container">
+            { pokemonData.map((pokemon) => {
+              return (
+                <Card 
+                  key={pokemon.id}
+                  name={pokemon.name}
+                  imgUrl={pokemon.sprites.other["official-artwork"].front_default}
+                  rank={pokemon.id}
+                  stats={pokemon.stats}
+                  pokeType={pokemon.types[0].type.name}
+                />
+              )
             })}
           </div>
+          <div className="btn">
+              <button onClick={prev}>Prev</button>
+              <button onClick={next}>Next</button>
+            </div>
           </>
         )
       }
---
+++ src/components/Card/Card.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import StatsBar from '../StatsBar/StatsBar'
+import './Card.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function Card({ imgUrl, name, rank, stats, pokeType }) {
+
+  const bgColor = {
+    normal: "#BDBDAF",
+    grass : "#7eb998",
+    fire:"#f09990",
+    water: "#8ab7e2",
+    fighting: "#cc8c7f",
+    flying: "#78A1FF",
+    poison: "#d183c8",
+    ground: " #f1db90",
+    rock : "#e4d699",
+    bug: "#d9e086",
+    ghost: "#9995e4",
+    electric: "#f1e48e",
+    psychic: "#e493bf",
+    ice : "#a2dee7",
+    dragon: "#9c8fe5",
+    dark : "#d8a992",
+    steel: "#a7a3d8",
+    fairy: "#ebacf0"
+  }
+
+  let color = "#000";
+  const keys = Object.keys(bgColor);
+  if(keys.includes(pokeType)) {
+      color = bgColor[pokeType];
+  }
+
+  function clicked() {
+
+  }
+
+  return (
+    <div className="card-container" onClick={clicked}>
+      <div className="card">
+        <div className="front-card" style = {{backgroundColor : color}}>
+          <div className="img-container">
+            <img src={ imgUrl } alt="" className="img-fluid"/>
+          </div>
+          <h4 className="rank"># { rank }</h4>
+          <h4>{ name }</h4>
+          <h4>{ pokeType }</h4>
+        </div>
+        {/* <div className="back-card">
+          <div className="stats">
+            { stats.map((pokemon, index) => (
+              <StatsBar 
+                key={index}
+                width = {pokemon.base_stat} 
+                statName= {pokemon.stat.name}
+              />
+          )) }
+          </div>
+        </div> */}
+      </div>
+    </div>
+  )
+}
+
+export default Card;
